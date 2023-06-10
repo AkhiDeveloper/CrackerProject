591a9b5c478954a510507250a17107a12f8d5c14
Akhilesh.mishra <akhileshmishra.developer@gmail.com>
2023-06-10 18:07:44 +0545

    update

---
+++ BookManager/BookManager.API/ServiceProvider/IFileStorage.cs
@@ -0,0 +1,11 @@
+﻿namespace BookManager.API.ServiceProvider
+{
+    public interface IFileStorage
+    {
+        Task UploadFile(string fileName, Stream stream, string folderPath = "");
+        Task DeleteFile(string fileName, string folderPath="");
+        Task DeleteFile(string filePath);
+        Task<Stream> DownloadFile(string fileName, string folderPath = "");
+        Task<Stream> DownloadFile(string filePath);
+    }
+}
---
+++ BookManager/BookManager.API/ServiceProvider/ImgurFileStorage.cs
@@ -0,0 +1,35 @@
+﻿namespace BookManager.API.ServiceProvider
+{
+    public class ImgurFileStorage
+        : IFileStorage
+    {
+        public ImgurFileStorage(string clientID, string clientSecret)
+        {
+
+        }
+        public Task DeleteFile(string fileName, string folderPath = "")
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteFile(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Stream> DownloadFile(string fileName, string folderPath = "")
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Stream> DownloadFile(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UploadFile(string fileName, Stream stream, string folderPath = "")
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
