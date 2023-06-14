f8a0211a1783f5420b640b325a7f342c9944158c
Akhilesh.mishra <akhileshmishra.developer@gmail.com>
2023-06-14 21:26:59 +0545

    FileStorageAdded

---
+++ BookManager/BookManager.API/ServiceProvider/LocalFileStorage.cs
@@ -0,0 +1,83 @@
+﻿namespace BookManager.API.ServiceProvider
+{
+    public class LocalFileStorage
+        : IFileStorage
+    {
+        private readonly string _basePath;
+
+        public LocalFileStorage(string basePath="Files")
+        {
+            if (!Directory.Exists(basePath))
+            {
+                Directory.CreateDirectory(basePath);
+            }
+            _basePath = basePath;
+        }
+
+        public Task DeleteFile(string fileName, string folderPath = "")
+        {
+            var targetFolderPath = Path.Combine(_basePath, folderPath);
+            if (!Directory.Exists(targetFolderPath))
+            {
+                throw new Exception("Folder not found!");
+            }
+            var targetFilePath = Path.Combine(targetFolderPath, fileName);
+            if (!File.Exists(targetFilePath))
+            {
+                throw new Exception("File not found");
+            }
+            File.Delete(targetFilePath);
+            return Task.CompletedTask;
+        }
+
+        public Task DeleteFile(string filePath)
+        {
+            var targetFilePath = Path.Combine(_basePath, filePath);
+            if (!File.Exists(targetFilePath))
+            {
+                throw new Exception("File not found");
+            }
+            File.Delete(targetFilePath);
+            return Task.CompletedTask;
+        }
+
+        public async Task<Stream> DownloadFile(string fileName, string folderPath = "")
+        {
+            var targetFolderPath = Path.Combine(_basePath, folderPath);
+            if (!Directory.Exists(targetFolderPath))
+            {
+                throw new Exception("Folder not found!");
+            }
+            var targetFilePath = Path.Combine(targetFolderPath, fileName);
+            if (!File.Exists(targetFilePath))
+            {
+                throw new Exception("File not found");
+            }
+            Stream fileStream = new FileStream(targetFilePath, FileMode.Open, FileAccess.Read);
+            return fileStream;
+        }
+
+        public async Task<Stream> DownloadFile(string filePath)
+        {
+            var targetFilePath = Path.Combine(_basePath, filePath);
+            if (!File.Exists(targetFilePath))
+            {
+                throw new Exception("File not found");
+            }
+            Stream fileStream = new FileStream(targetFilePath, FileMode.Open, FileAccess.Read);
+            return fileStream;
+        }
+
+        public async Task UploadFile(string fileName, Stream stream, string folderPath = "")
+        {
+            var targetFolderPath = Path.Combine(_basePath, folderPath);
+            if (!Directory.Exists(targetFolderPath))
+            {
+                Directory.CreateDirectory(targetFolderPath);
+            }
+            var targetFilePath = Path.Combine(targetFolderPath, fileName);
+            Stream fileStream = new FileStream(targetFilePath, FileMode.CreateNew, FileAccess.Write);
+            await stream.CopyToAsync(fileStream);
+        }
+    }
+}
