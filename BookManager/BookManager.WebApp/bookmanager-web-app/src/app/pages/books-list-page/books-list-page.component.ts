8b7e60d1ac5ae7e74979c5dd9c49e5cdbaac9746
Akhilesh Mishra <70856073+AkhiDeveloper@users.noreply.github.com>
2024-03-25 21:48:19 +0545

    added ng bootsrap

---
+++ BookManager/BookManager.WebApp/bookmanager-web-app/src/app/pages/books-list-page/books-list-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-books-list-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './books-list-page.component.html',
+  styleUrl: './books-list-page.component.scss'
+})
+export class BooksListPageComponent {
+
+}
