e7543f22b34365bcef5cb5926e905bef2a742271
Akhilesh Mishra <70856073+AkhiDeveloper@users.noreply.github.com>
2024-03-25 22:18:24 +0545

    added book list page

--- BookManager/BookManager.WebApp/bookmanager-web-app/src/app/app.routes.ts
+++ BookManager/BookManager.WebApp/bookmanager-web-app/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { BooksListPageComponent } from './pages/books-list-page/books-list-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'books',
+        component: BooksListPageComponent,
+        pathMatch: 'full'
+    },
+    {
+        path: '',
+        redirectTo: 'books',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: '',
+    }
+];
